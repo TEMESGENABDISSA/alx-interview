74d6de91732f60d182465f28fe4473a55922d3a8
Temesgen Abdissa <temesgenabdissa2@gmail.com>
2024-01-09 06:38:22 +0000

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,25 +1,44 @@
 #!/usr/bin/python3
-"""
-Method that determines if
-all the boxes can be opened
-"""
+"""Lockboxes"""
 
 
 def canUnlockAll(boxes):
-    """
-    function canUnlockAll
-    """
-    size_box = len(boxes)
-    keys = [0]
+    """Check if all boxes can be opened
 
-    for key in keys:
-        for box in boxes[key]:
-            if box < size_box:
-                if box not in keys:
-                    keys.append(box)
+    Args:
+        boxes (arr[int]): Boxes contains the key to open other boxes
 
-    if size_box == len(keys):
+    Return:
+        (bool): True otherwise false
+    """
+    unlocked = [0]
+    for boxID, box in enumerate(boxes):
+        if not box:
+            continue
+        for key in box:
+            if key < len(boxes) and key not in unlocked and key != boxID:
+                unlocked.append(key)
+    if len(unlocked) == len(boxes):
         return True
-
     return False
 
+
+def canUnlockAll2(boxes):
+    """Check if all boxes can be opened
+
+    Args:
+        boxes (arr[int]): Boxes contains the key to open other boxes
+
+    Return:
+        (bool): True otherwise false
+    """
+    unlocked = set()
+    for boxID, box in enumerate(boxes):
+        if len(box) == 0 or boxID == 0:
+            unlocked.add(boxID)
+        for key in box:
+            if key < len(boxes) and key != boxID:
+                unlocked.add(key)
+    if len(unlocked) == len(boxes):
+        return True
+    return False
